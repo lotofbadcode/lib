5a4282d775c2c5fef8f4dccfa4ef8f8efe950a56
代码庸医 <3359964266@qq.com>
2019-08-06 15:26:25 +0800

    数据库骨架

--- dbskeleton/ISkeleton.php
+++ dbskeleton/ISkeleton.php
@@ -14,25 +14,25 @@ interface ISkeleton
     /**
      * 修改表
      */
-    public function alterTable();
+    public function alterTable(TableModel $oldtableModel, TableModel $newtableModel);
 
     /**
      * 删除表
      */
-    public function dropTable();
+    public function dropTable(TableModel $tableModel);
 
     /**
      * 添加字段
      */
-    public function addColumn();
+    public function addColumn(TableModel $tableModel, ColumnModel $columnModel);
 
     /**
      * 修改字段
      */
-    public function changeColumn();
+    public function changeColumn(TableModel $tableModel, ColumnModel $oldcolumnModel, ColumnModel $newcolumnModel);
 
     /**
      * 删除字段
      */
-    public function dropColumn();
+    public function dropColumn(TableModel $tableModel, ColumnModel $columnModel);
 }
